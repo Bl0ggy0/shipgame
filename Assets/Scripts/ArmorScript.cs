b02a217266df6bde548c70a1feb99e67599bc371
LudiOgnjen7 <ognjengamesnk@gmail.com>
2026-08-24 19:37:08 +0200

    armor pocetak

---
+++ Assets/Scripts/ArmorScript.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ArmorScript : MonoBehaviour
+{
+    // 0=helmet | 1=chestplate | 2=leggings | 3=boots
+    public Sprite[] clothSprites;
+    public Sprite[] ironSprites;
+}
