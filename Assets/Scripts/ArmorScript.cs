a48ba91889e8152e82d8302dafd9f411843d85fb
LudiOgnjen7 <ognjengamesnk@gmail.com>
2026-08-27 22:01:49 +0200

    malo update armor

--- Assets/Scripts/ArmorScript.cs
+++ Assets/Scripts/ArmorScript.cs
@@ -2,7 +2,27 @@ using UnityEngine;
 
 public class ArmorScript : MonoBehaviour
 {
-    // 0=helmet | 1=chestplate | 2=leggings | 3=boots
+    // SPRITES | 0=helmet | 1=chestplate | 2=leggings | 3=boots
     public Sprite[] clothSprites;
     public Sprite[] ironSprites;
+
+    [Space]
+
+    // STATS
+    public float TotalDef;
+
+    private float HelmetDef;
+    private float ChestplateDef;
+    private float LeggingsDef;
+    private float BootsDef;
+
+    private void UpdateStats()
+    {
+        TotalDef = HelmetDef + ChestplateDef + LeggingsDef + BootsDef;
+    }
+
+    public void UnequipHelmet()
+    {
+
+    }
 }
